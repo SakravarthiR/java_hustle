2ea543a9dcb9d5ced1af17e627af523dee6139c8
ESP32 Developer <190198612+SakravarthiR@users.noreply.github.com>
2026-01-29 21:45:27 +0530

    Today Learn

--- lec_6/Decar.java
+++ lec_6/Decar.java
@@ -6,27 +6,27 @@ public class Decar{
         System.out.println(ans);
         
     }
-    static int binarySearch(int[] arr , int target){
+    static int binarySearch(int[] arr , int tar){
         int start =0;
         int end = arr.length-1;
 
         boolean isAsc = arr[start] < arr[end];
          while(start<=end){
             int mid = start + (end - start) /2;
-            if(arr[mid]==target){
+            if(arr[mid]==tar){
                 return mid;
             }
         if(isAsc){
-            if(target < arr[mid]){
-                end = mid - 1;
+            if(tar < arr[mid]){
+                end =  mid - 1;
             }else {
-                start = mid +1;
+                start =  mid +1;
             }
          }else{
-            if(target >arr[mid]){
+            if(tar >arr[mid]){
                 end = mid -1;
             }else{
-                start =mid +1;
+                start = mid +1;
             }
          }
     }
