dedf460eb78ba15956bdf61c69cb5d4ce3f0de41
ESP32 Developer <190198612+SakravarthiR@users.noreply.github.com>
2026-02-02 23:02:03 +0530

    Today

---
+++ lec_8/New.java
@@ -0,0 +1,21 @@
+
+    import java.util.Scanner;
+
+public class New {
+    public static void main(String[] args){
+        Scanner in  = new Scanner(System.in);
+        System.out.print("Enter Number:");
+        long num = in.nextInt();
+        long ans = 0;
+        
+        while (num > 0) {
+            long rem = num % 10;
+            num = num / 10;
+
+            ans = ans * 10 + rem;
+            
+        }
+        System.out.println("Your number has been reversed." + ans);
+    }
+}
+
